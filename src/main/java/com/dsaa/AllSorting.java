0be222481de363dcf37c5b0da493f4be113c0bdf
Lesley Thuto Johnson <whosekid@gmail.com>
2021-11-06 16:45:23 +0200

    Implementation of Bubble Sort ascending

--- src/main/java/com/dsaa/AllSorting.java
+++ src/main/java/com/dsaa/AllSorting.java
@@ -3,6 +3,18 @@ package com.dsaa;
 public class AllSorting<T extends Comparable<T>> {
 
     public T[] bubbleSortAsc(T[] arr){
+        T temp;
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - 1 -i; j++){
+                // if not in order
+                if ( arr[i].compareTo(arr[i+1]) > 0){
+                    // perfom a swap
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
         return arr;
     }
     public T[] bubbleSortDesc(T[] arr){
